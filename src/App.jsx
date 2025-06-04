2e3457bdb2eaa15d91ed895151bce721c6a65432
Gourab Patra <grvpatro@gmail.com>
2025-06-04 21:45:45 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import HeroSection from "./components/HeroSection" 
+
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <>
+      <Header />
+      <HeroSection />
+      </>
+    </h1>
+  )
+}
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+
+
+const HeroSection = () => {
+  return (
+    <section className="h-screen bg-gradient-to-b from-violet-900 to-black flex xl:flex-row flex-col-reverse items-center justify-between px-10 relative overflow-hidden">
+
+    </section>
+  )
+}
+
+export default HeroSection
